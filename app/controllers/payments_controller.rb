03816117606d321d83e7a9fe9c2f7c625bc2f4ac
Leena Chaudhari <chaudharileena169@gmail.com>
2018-02-13 19:10:00 +0530

    generate-checksum and verify-checksum API calls for mobile.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,7 @@ class PaymentsController < ApplicationController
   include PaytmHelper::EncryptionNewPG
   protect_from_forgery except: [:order_processed]
 
+  #WEB
   # post /proceed-to-pay
   def proceed_to_pay
 
@@ -30,6 +31,8 @@ class PaymentsController < ApplicationController
     @param_list["WEBSITE"] = Rails.application.secrets[:paytm_credentials][payment_environment][:web][:website]
     @param_list["CALLBACK_URL"] = Rails.application.secrets[:paytm_credentials][payment_environment][:web][:callback_url]
 
+    puts "---#{@param_list}---"
+    puts "---#{Rails.application.secrets[:paytm_credentials][payment_environment][:merchant_key]}---"
     @checksum_hash = new_pg_checksum(@param_list, Rails.application.secrets[:paytm_credentials][payment_environment][:merchant_key]).gsub("\n",'')
 
     puts "param_list: #{@param_list}"
@@ -43,4 +46,71 @@ class PaymentsController < ApplicationController
     puts "#{params.to_s}"
   end
 
+  # MOBILE
+  # post generate-checksum
+  def generate_checksum
+    payment_environment = :production #(params[:payment_environment] == "production" ? :production : :staging)
+
+    params_keys_to_accept = ["MID", "ORDER_ID", "CUST_ID", "INDUSTRY_TYPE_ID", "CHANNEL_ID", "TXN_AMOUNT",
+      "WEBSITE", "CALLBACK_URL", "MOBILE_NO", "EMAIL", "THEME"]
+    params_keys_to_ignore = ["USER_ID", "controller", "action", "format"]
+
+    paytmHASH = Hash.new
+
+    paytmHASH["MID"] = Rails.application.secrets[:paytm_credentials][payment_environment][:mid]
+    paytmHASH["ORDER_ID"] = params["ORDER_ID"]
+    paytmHASH["CUST_ID"] = params["CUST_ID"]
+    paytmHASH["INDUSTRY_TYPE_ID"] = Rails.application.secrets[:paytm_credentials][payment_environment][:industry_type_id]
+    paytmHASH["CHANNEL_ID"] = Rails.application.secrets[:paytm_credentials][payment_environment][:web][:channel_id]
+    paytmHASH["TXN_AMOUNT"] = params["TXN_AMOUNT"]
+    paytmHASH["WEBSITE"] = Rails.application.secrets[:paytm_credentials][payment_environment][:web][:website]
+
+    keys = params.keys
+    keys.each do |key|
+      if ! params[key].blank?
+        puts "params[#{key}] : #{params[key]}"
+        if !(params_keys_to_accept.include? key)
+            next
+        end
+        paytmHASH[key] = params[key]
+      end
+    end
+
+    mid = paytmHASH["MID"]
+    order_id = paytmHASH["ORDER_ID"]
+
+    Rails.logger.debug "paytmHASH: #{paytmHASH}"
+
+    checksum_hash = PaytmHelper::ChecksumTool.new.get_checksum_hash(paytmHASH, Rails.application.secrets[:paytm_credentials][payment_environment][:merchant_key]).gsub("\n",'')
+
+    # Prepare the return json.
+    returnJson = Hash.new
+    returnJson["CHECKSUMHASH"] =  checksum_hash
+    returnJson["ORDER_ID"]     =  order_id
+    returnJson["payt_STATUS"]  =  1
+
+    Rails.logger.debug "returnJson: #{returnJson}"
+
+    render json: returnJson
+  end
+
+  # post verify-checksum
+  def verify_checksum
+    payment_environment = :production #(params[:payment_environment] == "production" ? :production : :staging)
+    params_keys_to_ignore = ["USER_ID", "controller", "action", "format"]
+
+    paytmHASH = Hash.new
+
+    keys = params.keys
+    keys.each do |key|
+      if (params_keys_to_ignore.include? key)
+        next
+      end
+
+      paytmHASH[key] = params[key]
+    end
+    paytmHASH = PaytmHelper::ChecksumTool.new.get_checksum_verified_array(paytmHASH, Rails.application.secrets[:paytm_credentials][payment_environment][:merchant_key])
+
+    @response_value = paytmHASH.to_json.to_s.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "landing#index"
-  
+
+  # Web
   get "/checkout" => "checkout#index", as: :checkout
   post "/proceed-to-pay" => "payments#proceed_to_pay", as: :proceed_to_pay
   post "/order-processed" => "payments#order_processed", as: :order_processed
+
+  # Mobile
+  post "/generate-checksum" => "payments#generate_checksum"
+  post "/verify-checksum" => "payments#verify_checksum"
+
 end
